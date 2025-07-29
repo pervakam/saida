1af32801475fc18db2db2ca1d2646aa893676bd0
a.pervak <a.pervak@zebrains.team>
2025-07-29 21:40:41 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue()
   ],
+  base: '/saida/', // если используешь GitHub Pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
